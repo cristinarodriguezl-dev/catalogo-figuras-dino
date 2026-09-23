fb4f9017bb0e7727c5aa904b9332c58bc87644b3
cristinarodriguezl-dev <cristinarodriguez.cv@gmail.com>
2026-09-23 10:47:19 +0200

    feat: add list_pieces and test

--- catalog.py
+++ catalog.py
@@ -11,3 +11,11 @@ def add_piece(id, name, category, price, status, description):
 
     return {"id": id, "name": name, "category": category, "price": price, "status": status, "description": description}
 
+def list_pieces(catalog):
+    if not isinstance(catalog, list):
+        raise ValueError("El catálogo debe ser una lista")
+    
+    names = []
+    for piece in catalog:
+        names.append(piece["name"])
+    return names
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import catalog
+from catalog import add_piece, list_pieces
 
+#Prueba
+
+catalog = []
+piece1 = add_piece("1", "Tyrannosaurus Rex", "figura", -5, "disponible", "Figura usada en buen estado")
+catalog.append(piece1)
+
+print(list_pieces(catalog))
